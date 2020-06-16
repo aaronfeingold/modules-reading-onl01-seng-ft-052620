d6e893cf2c268c4034498450a9b850f27390ad8e
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-16 00:09:13 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,6 +5,9 @@ require_relative './fancy_dance.rb'
 class Dancer
   # include Dance
   # extend MetaDancing
+  
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
 
   attr_accessor :name
 
